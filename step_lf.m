93650e46bde060e7e430f46846da653ea4f12875
Landen Lloyd <oldestoflloyd@gmail.com>
2022-12-05 15:10:09 -0500

    Vectorize step_lf code for better performance

--- step_lf.m
+++ step_lf.m
@@ -7,20 +7,15 @@ function [bodies] = step_lf(bodies, masses, dt, softening)
 %   masses: a 1 x N matrix, where each row holds the mass for that body
 %   dt: the fixed timestep
 %   softening: the distance below which gravity is supressed
-[N, ~] = size(masses);
 
 % Integrate each body according to the first two steps in leapfrog
-for i = 1:N
-    bodies(i, 4:6) = bodies(i, 4:6) + bodies(i, 7:9) * dt / 2;
-    bodies(i, 1:3) = bodies(i, 1:3) + bodies(i, 4:6) * dt;
-end
+bodies(:, 4:6) = bodies(:, 4:6) + bodies(:, 7:9) * dt / 2;
+bodies(:, 1:3) = bodies(:, 1:3) + bodies(:, 4:6) * dt;
 
 bodies = get_accel(bodies, masses, softening);
 
 % Perform the last step of leapfrog
-for i = 1:N
-    bodies(i, 4:6) = bodies(i, 4:6) + bodies(i, 7:9) * dt / 2;
-end
+bodies(:, 4:6) = bodies(:, 4:6) + bodies(:, 7:9) * dt / 2;
 
 end
 
